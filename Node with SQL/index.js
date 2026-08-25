9c787379b059c473e208b27c013621d4efd1ad1b
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-08-25 22:22:43 +0530

    Insert data using placeholder

--- Node with SQL/index.js
+++ Node with SQL/index.js
@@ -10,26 +10,33 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
 });
 
-
-
 //Now our connection in build, how to use it?
 // Use query method available in connection.
 
+// Using placeholders
+// try {
+//   const [results] = await connection.query(
+//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//     ['Page', 45]
+//   );
+let query = "Insert into user(id,username,email,password) values (?,?,?,?)";
+let user = ["123", "123_newuser", "abc@gmail.com", "abc"]; //We want these values are come on the place of placeholders.
+// We can also make nested array 
+// connection.query(query,user, (err, result); We can pass in this way.
+
 try {
-  connection.query("Show tables", (err, result) => {
+  connection.query(query, user, (err, result) => {
     //We can also write this query in a variable and pass in it.
     if (err) throw err;
     console.log(result); //[ { Tables_in_delta_app: 'temp' } ] //This result is actually an array.
     //After the execution of this we can observe our connection doesn't stop.
-    console.log(result.length);
-    console.log(result[0]);
+    // console.log(result.length);
+    // console.log(result[0]);
     connection.end(); //to close connection.
   });
 } catch (err) {
   console.log(err);
 }
-
-
 
 let getRandomUser = () => {
   return {
