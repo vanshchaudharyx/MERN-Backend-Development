758a3982f2e6259701d6666a0540aef6453b94aa
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-08-25 21:42:29 +0530

    Add MySQL connection

--- Node with SQL/index.js
+++ Node with SQL/index.js
@@ -1,4 +1,31 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+
+
+//Now our connection in build, how to use it?
+// Use query method available in connection.
+try {
+  connection.query("Show tables", (err, result) => {
+    if (err) throw err;
+    console.log(result); //[ { Tables_in_delta_app: 'temp' } ]
+    //After the execution of this we can observe our connection doesn't stop.
+    connection.end(); //to close connection.
+  });
+} catch (err) {
+  console.log(err);
+}
+
+
 
 let getRandomUser = () => {
   return {
@@ -11,4 +38,4 @@ let getRandomUser = () => {
     // registeredAt: faker.date.past(),
   };
 };
-console.log(getRandomUser());
+// console.log(getRandomUser());
