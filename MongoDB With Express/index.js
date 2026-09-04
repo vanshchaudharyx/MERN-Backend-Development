04cb62796551c248faac8ddda13a1ba53479fefc
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-04 22:18:12 +0530

    Creating chat model

--- MongoDB With Express/index.js
+++ MongoDB With Express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chats.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -11,6 +12,20 @@ main().then(() => {
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+let chat1 = new Chat({
+  from: "neha",
+  to: "priya",
+  msg: "send me your sheets",
+  created_at: new Date(), //This is a date class that generate a random date.
+});
+chat1.save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
   res.send("root is working");
 });
