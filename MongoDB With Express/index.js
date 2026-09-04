21f152466101bfecd33883872ba0aff8433f0173
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-04 21:04:05 +0530

    Basic app setup

---
+++ MongoDB With Express/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main().then(() => {
+  console.log("Connection Successful");
+});
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+app.get("/", (req, res) => {
+  res.send("root is working");
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
