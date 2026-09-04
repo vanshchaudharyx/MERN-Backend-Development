f188f22c7830e349aaa2eb0c07844c2602fb6ce1
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-04 22:54:54 +0530

    /chats route

--- MongoDB With Express/index.js
+++ MongoDB With Express/index.js
@@ -12,20 +12,30 @@ main().then(() => {
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
-let chat1 = new Chat({
-  from: "neha",
-  to: "priya",
-  msg: "send me your sheets",
-  created_at: new Date(), //This is a date class that generate a random date.
-});
-chat1.save()
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// let chat1 = new Chat({
+//   from: "neha",
+//   to: "priya",
+//   msg: "send me your sheets",
+//   created_at: new Date(), //This is a date class that generate a random date.
+// });
+// chat1
+//   .save()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
+// Index route
+// Get /chats--> show all chats.
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find(); //This is function that take data from database , it is an asynchronus funtion and return a promise.
+  // console.log(chats);
+  // console.log(chats[0].from);
+  // res.send("working");
+  res.render("index.ejs", { chats });
+});
 app.get("/", (req, res) => {
   res.send("root is working");
 });
