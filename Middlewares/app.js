5904436e771bae66bb1744febf804c9c98b5dfe5
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-10 16:26:24 +0530

    Using next()

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -4,9 +4,17 @@ const app = express();
 //Middleware-->response send
 // app.use(middleware)
 //app.use executes this with every incoming request. so it sends same response to every route,thats why we dont reach to the next paths.
-app.use((req, res) => {
-  console.log("Hii i m middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
-  res.send("Middleware finished");
+app.use((req, res, next) => {
+  console.log("Hii i m 1st middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
+  //   res.send("Middleware finished");
+  next();
+  //   console.log("This is after next()");--> This shows u r not a giid progrmmer
+});
+
+app.use((req, res, next) => {
+  console.log("Hii i m 2nd middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
+  //   res.send("Middleware finished");
+  next();
 });
 
 app.get("/", (req, res) => {
