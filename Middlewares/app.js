68c5a2ff36e0e7b2b3709394f472f89b4eef0fe0
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-10 16:45:28 +0530

    Creating utility middleware

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -4,17 +4,22 @@ const app = express();
 //Middleware-->response send
 // app.use(middleware)
 //app.use executes this with every incoming request. so it sends same response to every route,thats why we dont reach to the next paths.
-app.use((req, res, next) => {
-  console.log("Hii i m 1st middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
-  //   res.send("Middleware finished");
-  next();
-  //   console.log("This is after next()");--> This shows u r not a giid progrmmer
-});
+// app.use((req, res, next) => {
+//   console.log("Hii i m 1st middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
+//   //   res.send("Middleware finished");
+//   next();
+//   //   console.log("This is after next()");--> This shows u r not a giid progrmmer
+// });
 
+// app.use((req, res, next) => {
+//   console.log("Hii i m 2nd middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
+//   //   res.send("Middleware finished");
+//   next();
+// });
+//Logger
 app.use((req, res, next) => {
-  console.log("Hii i m 2nd middleware"); // If this sends a response then middlewares stop or it can call next middleware or next control.
-  //   res.send("Middleware finished");
-  next();
+  req.Time = new Date(Date.now()).toString();
+  console.log(req.method, req.path, req.time, req.hostname);
 });
 
 app.get("/", (req, res) => {
