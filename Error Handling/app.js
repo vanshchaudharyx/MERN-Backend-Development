530809b1ceab7bcd5698330c78d6ff78e8ff3031
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-12 20:04:32 +0530

    Default status and message

--- Error Handling/app.js
+++ Error Handling/app.js
@@ -7,11 +7,11 @@ app.get("/err", (req, res) => {
 });
 
 //Error Handling Middleware-->
-app.use((err, req, res, next) => {
-  console.log("======");
-  //   next(); //Whenever we call next then the next will search for non error handling middleware.
-  next(err); //If we want to trigger the express's defalut error handler
-});
+// app.use((err, req, res, next) => {
+//   console.log("======");
+//   //   next(); //Whenever we call next then the next will search for non error handling middleware.
+//   //   next(err); //If we want to trigger the express's defalut error handler
+// });
 
 const checkToken = (req, res, next) => {
   let { token } = req.query;
@@ -22,6 +22,12 @@ const checkToken = (req, res, next) => {
 };
 app.get("/api", checkToken, (req, res) => {
   res.send("data");
+});
+app.use((err, req, res, next) => {
+  //   console.log("==Error==");
+  //   res.send(err);
+  let { status, message } = err;
+  res.status(status).send(message);
 });
 
 app.listen(8080, () => {
