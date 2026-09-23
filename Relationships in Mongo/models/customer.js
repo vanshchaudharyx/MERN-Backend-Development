9462aed6a52b25aa94b5100ffa88a59bdec250e6
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-09-23 21:37:57 +0530

    Understanding Pre and Post middleware for deletion

--- Relationships in Mongo/models/customer.js
+++ Relationships in Mongo/models/customer.js
@@ -25,7 +25,18 @@ const customerSchema = new Schema({
     },
   ],
 });
-
+// customerSchema.pre("findOneAndDelete", async () => {
+//   console.log("Pre middleware");
+// });
+// When this pre middleware triggers then post middleware auto trigger
+customerSchema.post("findOneAndDelete", async (customer) => {
+  console.log("Post middleware");
+  // console.log(data);
+  if (customer.order.length) {
+    let result = await Order.deleteMany({ _id: { $in: customer.order } });
+    console.log("Deleted Orders",result);
+  }
+});
 const Customer = mongoose.model("Customer", customerSchema);
 
 const addCustomer = async () => {
@@ -39,23 +50,48 @@ const addCustomer = async () => {
   let result = await cust1.save();
   console.log(result);
 };
-addCustomer();
+// addCustomer();
 
-// const addOrders = async () => {
-//   let result = await Order.insertMany([
-//     {
-//       item: "Samosa",
-//       price: 12,
-//     },
-//     {
-//       item: "Chips",
-//       price: 10,
-//     },
-//     {
-//       item: "Choclate",
-//       price: 40,
-//     },
-//   ]);
-//   console.log(result);
-// };
+const addOrders = async () => {
+  let result = await Order.insertMany([
+    {
+      item: "Samosa",
+      price: 12,
+    },
+    {
+      item: "Chips",
+      price: 10,
+    },
+    {
+      item: "Choclate",
+      price: 40,
+    },
+  ]);
+  console.log(result);
+};
 // addOrders();
+
+//Functions->
+const addCust = async () => {
+  let newCust = new Customer({
+    name: "Karan Arjun",
+  });
+  let newOrder = new Order({
+    item: "Burger",
+    price: 250,
+  });
+  newCust.order.push(newOrder);
+  await newOrder.save();
+  await newCust.save();
+  console.log("Added new customer");
+};
+// addCust();
+
+const delCust = async () => {
+  let data = await Customer.findByIdAndDelete("6ab3f7ece18207468b56da37"); //This only delete the customer not the order.
+  console.log("Deleted Customer",data);
+  // There is no middleware for findByIdAndDelete but findOneAndDelete exists and this itself is a middleware
+  // So if findbyIdAndDelete trigers findOneAndDelete then modngoose middleware automatically triggers.
+};
+
+delCust();
